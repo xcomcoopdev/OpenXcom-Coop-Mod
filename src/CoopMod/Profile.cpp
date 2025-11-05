73114fc1312cc33205e074ca3d4c65af52dd34ec
xcomcoopdev <xcom.coop.dev@gmail.com>
2025-11-05 16:08:05 +0200

    OXCE v8.4

--- src/CoopMod/Profile.cpp
+++ src/CoopMod/Profile.cpp
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+ * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>. 
  */
 #include "../Basescape/SoldierArmorState.h"
 #include "../Basescape/SoldierAvatarState.h"
